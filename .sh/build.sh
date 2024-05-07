6b220c40ba44417d8200191365e101c1ad67098b
lithdoo <130959182+lithdoo@users.noreply.github.com>
2024-05-08 01:07:05 +0800

    Revert "refactor: 修改目录结构"
    This reverts commit 5823ef0a6741cd70298e3aa9d871765441c8e654.

--- .sh/build.sh
+++ .sh/build.sh
@@ -4,10 +4,10 @@ cd $(dirname $0)
 cd ../server/
 cargo build --release
 
-mv ./target/release/control-center-server ../resources/server
+mv ./target/release/control-center-server ../client/resources/server
 rm -rf ./target
 
-cd ../
+cd ../client
 pnpm install --no-frozen-lockfile
 pnpm run build:linux
 
