d46a5dec8f134bf5f3c1c9f48980228c7d97b28d
lithdoo <130959182+lithdoo@users.noreply.github.com>
2024-05-26 02:37:58 +0800

    Update build.bat

--- .bat/build.bat
+++ .bat/build.bat
@@ -2,4 +2,4 @@ cd %~dp0..\server
 cargo build --release 
 move .\target\release\control-center-server.exe ..\client\resources\server
 del  .\target /Q
-..\client\resources\server
+@REM ..\client\resources\server
